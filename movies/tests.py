f572bed0f845396262ab25d86c9f13aee1285330
tkosudi <tkosudi@gmail.com>
2021-09-30 12:02:36 -0300

    fix: Test DELETE route was builded and working.

--- movies/tests.py
+++ movies/tests.py
@@ -191,3 +191,37 @@ class MoviesTest(APITestCase):
         
         response = self.client.get('/api/movies/1/', format='json')
         self.assertEqual(response_data, response.json())
+
+
+    def test_delete_retrieve_movie_view_success(self):
+        superuser_sign_in_data = {
+            "username":"uRetrieveDestroyAPIViewser",
+            "password":"1234",
+            "first_name":"John",
+            "last_name":"Wick",
+            "is_superuser":True,
+            "is_staff":True,
+        }
+        self.client.post('/api/accounts/', superuser_sign_in_data, format='json')
+
+        superuser_sign_up_data = {
+            "username":"user",
+            "password":"1234",
+        }
+        self.client.post('/api/login/', superuser_sign_up_data, format='json')
+
+        movie_data = {
+            "title": "O Poderoso Chefão 2",
+            "duration": "175m",
+            "genres": [
+                {"name": "Crime"},
+                {"name": "Drama"}
+            ],
+            "premiere": "1972-09-10",
+            "classification": 14,
+            "synopsis": "Don Vito Corleone (Marlon Brando) é o chefe de uma 'família' ..."
+        }
+        self.client.post('/api/movies/', movie_data, format='json')
+
+        response = self.client.delete('/api/movies/1/', format='json')
+        self.assertEqual(response.status_code, 204)
