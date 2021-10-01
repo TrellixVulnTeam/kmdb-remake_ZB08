e14fd9937dc77780088ee5a8ab5baab40a59075d
Thiago Kosudi <tkosudi@gmail.com>
2021-10-01 12:50:25 -0300

    Feature/tests (#3)
    App Movies was built and tested

--- kmdb/settings.py
+++ kmdb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zyt0(dbio!tlhao2k$lg-t1b-$-=ss2fiznau3w=ah2c*_052j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    duration = models.CharField(max_length=5)
+    premiere = models.CharField(max_length=10)
+    classification = models.IntegerField()
+    synopsis = models.TextField()
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    movie = models.ManyToManyField(Movie, related_name='genres')
+
+class Review(models.Model):
+    stars = models.IntegerField()
+    review = models.TextField()
+    spoilers = models.BooleanField()
+    critic = models.ForeignKey(User, on_delete=models.CASCADE, related_name='critic_reviews', null=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews', null=True)
+
