173a00af555a70170b5ea52bce57bd070dead79c
tkosudi <tkosudi@gmail.com>
2021-10-05 12:49:32 -0300

    feat: PUT /api/movies/<int:movie_id>/review/ done!

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import MovieView, RetrieveMovieView
+from .views import MovieView, RetrieveMovieView, ReviewView
 
 urlpatterns = [
     path('movies/', MovieView.as_view()),
-    path('movies/<int:movie_id>/', RetrieveMovieView.as_view())
+    path('movies/review/', ReviewView.as_view()),
+    path('movies/<int:movie_id>/', RetrieveMovieView.as_view()),
+    path('movies/<int:movie_id>/review/', ReviewView.as_view()),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,11 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .permissions import AdminPermission, CriticPermission, IsAdminCriticOrReadOnly
 from rest_framework.authentication import TokenAuthentication
 from django.shortcuts import get_object_or_404
-from .models import Movie
-from .serializers import FullMovieSerializer, MovieSerializer
-
+from .models import Movie, Review
+from .serializers import FullMovieSerializer, MovieSerializer, ReviewSerializer
+import ipdb
 
 class MovieView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
@@ -23,8 +23,46 @@ class RetrieveMovieView(generics.RetrieveDestroyAPIView):
     permission_classes = [IsAdminCriticOrReadOnly]
 
     def get_serializer_class(self): # Seleciona a classe do serializer de acordo com a condicional.
-        if self.request.user.is_staff or self.request.user.is_superuser:
+        if self.request.user.is_authenticated:
             return FullMovieSerializer
         return MovieSerializer
 
-    
+class ReviewView(generics.CreateAPIView, generics.UpdateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_url_kwarg = 'movie_id'
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [CriticPermission]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # Desenvolver a lógica que atenda os requisitos propostos
+
+        # TODO Verificar se já existe uma review associada ao crítico, se o movie_id é valido
+        review = Review.objects.filter(critic=request.user.id, movie=kwargs['movie_id']) 
+        movie = get_object_or_404(Movie, pk=kwargs['movie_id'])
+
+        # TODO se não existir criar essa review
+        if len(review) == 0:
+            # Passar a response para o ReviewSerializer
+            create_review = Review.objects.create(**request.data, critic=request.user, movie=movie)
+            serializer = ReviewSerializer(create_review)
+            
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # ! Se já existir retornar uma mensagem padrão do material
+        return Response({"detail": "You already made this review."}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False) #PARTIAL FALSE == PUT PARTIAL TRUE==PATCH
+        review = get_object_or_404(Review, critic=request.user.id, movie=kwargs['movie_id']) # VERIFICANDO SE A REVIEW EXISTE E INSTANCIANDO
+        serializer = self.get_serializer(review, data=request.data, partial=partial) # SERIALIZANDO A REVIEW
+        serializer.is_valid(raise_exception=True) # VERIFICANDO SE O SERIALIZER É VALIDO
+
+        self.perform_update(serializer) # FAZENDO E SALVANDO AS ALTERAÇÕES PROPOSTAS
+
+        return Response(serializer.data) # RETORNANDO SERIALIZER DATA, COM HTTP RESPONSE 200 (PADRÃO)
+
+
